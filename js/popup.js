105d3742a49189ef68502ebabac88bcf82d216e1
Xavier Duthil <x.duthil@gmail.com>
2018-12-01 01:49:32 +0100

    firefox: Hide settings button (shortcuts are not configurable)

--- js/popup.js
+++ js/popup.js
@@ -131,11 +131,6 @@ function addEvents(currentTab) {
     refreshPopup(currentTab);
   });
 
-  // Add click event to the settings button
-  document.getElementById('settingsButton').addEventListener("click",  function () {
-    chrome.tabs.create({url: 'chrome://extensions/configureCommands'});
-  });
-
   // Add click events to the tracklist
   tracklistTable.addEventListener("click", function (event) {
     var clickedTrElement = event.target.parentElement;
